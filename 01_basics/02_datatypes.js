9893823ed8723b8babca12efaf619e4b4015b15b
Raazia Imran Reshamwala <raaziaimran2024@gmail.com>
2025-09-09 12:34:34 +0000

    dataTypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat old JS Code as a newer version
+
+// alert (3+3) // we are using nodejs not browser
+
+let name = "Raazia"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
