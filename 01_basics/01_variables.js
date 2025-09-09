2c66316d5f07a52ee3e276d67a70d02ad32f9067
Raazia Imran Reshamwala <raaziaimran2024@gmail.com>
2025-09-09 12:19:18 +0000

    variables and constants done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 55555
+let accountEmail = "rir@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+let accountState;
+
+//accountId = 11111 ---> //not allowed
+
+/*
+prefer not to use var bcuz of issue in block scope and functional scope
+*/
+
+
+
+console.log(accountPassword);
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState])
